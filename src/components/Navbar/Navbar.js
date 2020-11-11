1d5b2f8003d79bebd35d54615eda2993a957dbfa
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-11-12 08:24:50 +1100

    create react navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { MenuItems } from './MenuItems'
+import { Button } from '../Button'
 import './Navbar.css'
 
+
 class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return(
             <nav className='NavbarItems'>
                 <h1 className="navbar-logo">React<i className='fab fa-react'></i></h1>
-                <div className="menu-icon">
-                    
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index)=>{
                         return (
                             <li key={index}>
@@ -21,6 +29,7 @@ class Navbar extends Component {
                         )
                     })}
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         )
     }
